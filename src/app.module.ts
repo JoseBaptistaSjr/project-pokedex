e274902fc7d08a5814a3c79f83cf1caa3a9890d4
DESKTOP-3GNGHHP\junim <jose.baptistasjr@gmail.com>
2024-06-17 05:06:29 -0300

    fé em JAH

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    PokemonsModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'pokemon',
-      database: 'pokedex',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: true,
     }),
+    PokemonsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
